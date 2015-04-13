229db661d00c2ac667a00aabd612dbe36bd34f20
nudeldunker <nudeldunker@go4more.de>
2015-04-13 15:47:46 +0200

    b

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -89,15 +89,70 @@ MainWindow::decodeCmd()
      int b=m_CmdList[i] & 0x380;
 
      int ByteCmd=m_CmdList[i] & 0x3F00;
+     int BitCmd=m_CmdList[i] & 0x3C00;
+     int ShrtCmd=m_CmdList[i] & 0x3800;
 
      if(ByteCmd == 0x0700 )
-        qDebug() << "ADDWF" <<i; //ADDWF(d,f);
+        qDebug() << "ADDWF" <<i; //ADDWF(f,d);
      else if(ByteCmd == 0x0500)
-         qDebug() << "ANDWF(d,f)" << i;
+         qDebug() << "ANDWF(f,d)" << i;
+     else if(ByteCmd == 0x0100)
+         CLRF(f);
+     else if((m_CmdList[i] & 0x3F80) == 0x0100)
+         CLRW();
+     else if(ByteCmd == 0x0900)
+         COMF(f,d);
+     else if(ByteCmd == 0x0300)
+         DECF(f,d);
+     else if(ByteCmd == 0x0B00)
+         DECFSZ(f,d);
+     else if(ByteCmd == 0x0A00)
+         INCF(f,d);
+     else if(ByteCmd == 0x0F00)
+         INCFSZ(f,d);
+     else if(ByteCmd == 0x0400)
+         IORWF(f,d);
+     else if(ByteCmd == 0x0800)
+         MOVF(f,d);
+     else if(ByteCmd == 0x0000)
+         MOVWF(f);
+     else if((m_CmdList[i] & 0x3F9F) == 0x0000)
+         NOP();
+     else if(ByteCmd == 0x0D00)
+         RLF(f,d);
+     else if(ByteCmd == 0x0C00)
+         RRF(f,d);
+     else if(ByteCmd == 0x0200)
+         SUBWF(f,d);
+     else if(ByteCmd == 0x0E00)
+         SWAPF(f,d);
+     else if(ByteCmd == 0x0600)
+         XORWF(f,d);
+
+     else if(BitCmd == 0x1000)
+         BCF(f,b);
+     else if(BitCmd == 0x1400)
+         BSF(f,b);
+     else if(BitCmd == 0x1800)
+         BTFSC(f,b);
+     else if(BitCmd == 0x1C00)
+         BTFSS(f,b);
+
+     else if((m_CmdList[i] & 0x3E00 ) == 0x3E00)
+        ADDLW(k);
+     else if(ByteCmd == 0x3900)
+         ANDLW(k);
+     else if(ShrtCmd == 0x2000)
+         CALL(k_long);
+     else if((m_CmdList[i]& 0XFFFF) == 0x0064)
+         CLRWDT();
+     else if(ShrtCmd == 0x2800)
+         GOTO(k_long);
 
 
-    if((m_CmdList[i] & 0x3800 ) == 0x2800)
-        qDebug()<< "GOTO " << i;
+
+
+
     }
 }
 
